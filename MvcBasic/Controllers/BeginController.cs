5495214138d388bd1f66ba053b16261731caefe2
volvic <jhontaku09@gmail.com>
2020-05-18 12:44:06 +0900

    プロジェクト ファイルを追加します。

---
+++ MvcBasic/Controllers/BeginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBasic.Models;
+using System.Diagnostics;
+
+namespace MvcBasic.Controllers
+{
+    public class BeginController : Controller
+    {
+      // GET: Begin
+      public ActionResult List()
+      {
+          return View(new List<MvcBasic.Models.Member>() { new Member() { Id = 0, Name = "test", Birth = DateTime.Now, Coment = "Coment Comment" }  });
+      }
+    }
+}
---
+++ MvcBasic/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcBasic.Models
+{
+  public class Member
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Birth { get; set; }
+    public string Coment { get; set; }
+  }
+}
